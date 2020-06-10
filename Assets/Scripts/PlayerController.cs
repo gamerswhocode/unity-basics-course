eb4d82a5c87b7e830ec439c38b66ce2646f31e81
Luis Ochoa <ph.lwyz@gmail.com>
2020-06-09 21:25:27 -0700

    Subiendo el proyecto completo visto en el curso

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        if (horizontalMovement != 0)
+        {
+            float moveX = horizontalMovement * Time.deltaTime * moveSpeed;
+            transform.Translate(moveX, 0, 0);
+        }
+
+        float verticalMovement = Input.GetAxis("Vertical");
+        if (verticalMovement != 0)
+        {
+            float moveY = verticalMovement * Time.deltaTime * moveSpeed;
+            transform.Translate(0, moveY, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetTrigger("rotate");
+        }
+    }
+}
